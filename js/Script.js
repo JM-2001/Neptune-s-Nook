9553f542c750306fbd4df9504ffa5da0ff7d3020
Jonathan <j_moreno@uncg.edu>
2024-02-29 22:57:14 -0500

    Milestone 3 update

--- js/Script.js
+++ js/Script.js
@@ -11,3 +11,58 @@ function displayBottomNav() {
     }
 }
 
+let createProductBoolean = false;
+
+function displayCreateProduct() {
+    const addProductForm = document.getElementById('product-listing-add');
+
+    if (createProductBoolean === true) {
+        addProductForm.classList.remove("product-listing-add-hidden");
+        addProductForm.classList.add("product-listing-add-show");
+        createProductBoolean = false;
+    } else {
+        addProductForm.classList.add("product-listing-add-hidden");
+        addProductForm.classList.remove("product-listing-add-show");
+        createProductBoolean = true;
+    }
+}
+
+function hideCreateProduct() {
+    const addProductForm = document.getElementById('product-listing-add');
+    addProductForm.classList.add("product-listing-add-hidden");
+    addProductForm.classList.remove("product-listing-add-show");
+    createProductBoolean = true;
+}
+
+let editProductBoolean = false;
+
+function makeProductEdit() {
+    const productDiv = document.getElementById('product-listing-1');
+    const productSaveBtn = document.querySelector('#product-listing-1 .listing-btn-div .save-btn');
+    const productGenBtn = document.querySelector('#product-listing-1 .listing-btn-div .edit-btn');
+    const productInputs = document.querySelectorAll('#product-listing-1 .listing-div input');
+    const productInputId = document.getElementById('product-id');
+    const productTextarea = document.getElementById('product-desc');
+
+    if (editProductBoolean === true) {
+        productSaveBtn.classList.remove('hidden');
+        productGenBtn.textContent = "Cancel";
+        for (let i = 0; i < productInputs.length; i++) {
+            let element = productInputs[i];
+            element.removeAttribute('readonly');
+        }
+        productTextarea.removeAttribute('readonly');
+        productInputId.removeAttribute('readonly');
+        editProductBoolean = false;
+    } else {
+        for (let i = 0; i < productInputs.length; i++) {
+            let element = productInputs[i];
+            element.setAttribute('readonly', 'readonly');
+        }
+        productTextarea.setAttribute('readonly', 'readonly');
+        productInputId.setAttribute('readonly', 'readonly');
+        productSaveBtn.classList.add('hidden');
+        productGenBtn.textContent = "Edit";
+        editProductBoolean = true;
+    }
+}
