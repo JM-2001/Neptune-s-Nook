c37e5abb0e03ce0419232015e3b1f773e6b479bb
Jonathan <j_moreno@uncg.edu>
2024-03-27 20:36:01 -0400

    Milestone 4: Uploading sql scripts

---
+++ drop_tables.sql
@@ -0,0 +1,14 @@
+-- Drop CartProducts table 
+DROP TABLE CartProducts;
+
+-- Drop Carts table 
+DROP TABLE Cart;
+
+-- Drop Products table 
+DROP TABLE Products;
+
+-- Drop Categories table
+DROP TABLE Categories;
+
+-- Drop Users table
+DROP TABLE Users;
